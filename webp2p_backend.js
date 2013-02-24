10e3210e4d1ae732d6b389b8243834c47eb3c128
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2013-02-24 02:28:46 +0100

    Loading webp2p library from inside WebWorker

--- webp2p_backend.js
+++ webp2p_backend.js
@@ -1,6 +1,21 @@
 // Based on code from https://github.com/jaredhanson/jsonrpc-postmessage
 
 importScripts(
+    '../webp2p/peersManager.js',
+
+    '../webp2p/handshake/index.js',
+    '../webp2p/handshake/PubNub.js',
+    '../webp2p/handshake/simpleSignaling.js',
+
+    '../webp2p/lib/datachannel.js',
+    '../webp2p/lib/EventTarget.js',
+    '../webp2p/lib/pubnub-3.3.min.js',
+    '../webp2p/lib/simpleSignaling.js',
+
+    '../webp2p/transport/index.js',
+    '../webp2p/transport/routing.js')
+
+importScripts(
     'bitmap.js',
     'cacheBackup.js',
     'db.js',
