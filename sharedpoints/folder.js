fe543e31306fb3fb9b4e3da14f20494561f28685
Jesús Leganés Combarro "Piranna <piranna@gmail.com>
2013-04-27 18:03:57 +0200

    Put hash() method in common for all sharedpoints
    Now it's only required a custom fileList() method for each sharedpoint
    that return a static array of fileentry objects with the current disk
    status each time is called, so it can be processed later asynchronously.

--- sharedpoints/folder.js
+++ sharedpoints/folder.js
@@ -12,101 +12,34 @@ _priv.Folder = function(fileList, db, filesManager)
   this.type = 'folder'
   this.size = 0
 
-  this.hash = function()
+  this.fileList = function()
   {
-    console.info("Hashing sharedpoint '"+this.name+"'...")
+    // Get the file entries from the FileList
+    var files = []
 
-    var self = this
-
-    // Get all files currently indexed to delete removed old ones
-    db.files_getAll_byPeer("", function(error, fileentries)
+    for(var i=0, file; file=fileList[i]; i++)
     {
-      function sort_pathName(a,b)
+      // Remove invalid entries
+      if(file.name == '.')
+        continue
+
+      // Generate the fileentry
+      var fileentry =
       {
-        return a.path.localeCompare(b.path) || a.name.localeCompare(b.name)
+        sharedpoint: this.name,
+        path: file.webkitRelativePath.split('/').slice(1, -1).join('/'),
+        name: file.name,
+
+        lastModifiedDate: file.lastModifiedDate,
+        file: file
       }
 
-      // Get entries only for this sharedpoint
-      var entries = []
+      files.push(fileentry)
+    }
 
-      for(var i=0, fileentry; fileentry=fileentries[i]; i++)
-        if(fileentry.sharedpoint == self.name)
-          entries.push(fileentry)
+    files.sort(this.sort_pathName)
 
-      entries.sort(sort_pathName)
-
-      // Get the file entries from the FileList
-      var files = []
-
-      for(var i=0, file; file=fileList[i]; i++)
-      {
-        // Remove invalid entries
-        if(file.name == '.')
-          continue
-
-        // Generate the fileentry
-        var fileentry =
-        {
-          sharedpoint: self.name,
-          path: file.webkitRelativePath.split('/').slice(1, -1).join('/'),
-          name: file.name,
-
-          lastModifiedDate: file.lastModifiedDate,
-          file: file
-        }
-
-        files.push(fileentry)
-      }
-
-      files.sort(sort_pathName)
-
-      // Run over the old and new files looking for deleted and added ones
-      var ai = bi= 0;
-      while(ai < entries.length || bi < files.length)
-      {
-        var entry = entries[ai]
-        var file  = files[bi]
-
-        // Entry was removed
-        if(entry < file || !file)
-        {
-          filesManager.delete(entry)
-
-          ai++;
-        }
-
-        // File was added
-        else if(entry > file || !entry)
-        {
-          filesManager.add(file)
-
-          bi++;
-        }
-
-        // Both entry and file are equal, check the modification date
-        else
-        {
-          // [Hack] When (re)moving the file from its original place, Chrome
-          // show it with size = 0 and lastModifiedDate = null instead of
-          // raising a NotFoundError error
-          if(file.lastModifiedDate == null)
-            filesManager.delete(entry)
-
-          else if(entry.lastModifiedDate < file.lastModifiedDate)
-            filesManager.add(file)
-
-          ai++;
-          bi++;
-        }
-      }
-
-      setTimeout(function()
-      {
-        self.hash()
-      },
-      5*1000)
-//      1*60*1000)
-    })
+    return files
   }
 }
 
--- sharedpoints/index.js
+++ sharedpoints/index.js
@@ -5,6 +5,81 @@ _priv.Sharedpoint = function(db, filesManager)
 {
   var self = this
 
+  this.sort_pathName = function(a,b)
+  {
+    return a.path.localeCompare(b.path) || a.name.localeCompare(b.name)
+  }
+
+  this.hash = function()
+  {
+    console.info("Hashing sharedpoint '"+this.name+"'...")
+
+    var self = this
+
+    // Get all files currently indexed to delete removed old ones
+    db.files_getAll_byPeer("", function(error, fileentries)
+    {
+      // Get entries only for this sharedpoint
+      var entries = []
+
+      for(var i=0, fileentry; fileentry=fileentries[i]; i++)
+        if(fileentry.sharedpoint == self.name)
+          entries.push(fileentry)
+
+      entries.sort(self.sort_pathName)
+
+      // Get the file entries from the FileList
+      var files = self.fileList()
+
+      // Run over the old and new files looking for deleted and added ones
+      var ai = bi= 0;
+      while(ai < entries.length || bi < files.length)
+      {
+        var entry = entries[ai]
+        var file  = files[bi]
+
+        // Entry was removed
+        if(entry < file || !file)
+        {
+          filesManager.delete(entry)
+
+          ai++;
+        }
+
+        // File was added
+        else if(entry > file || !entry)
+        {
+          filesManager.add(file)
+
+          bi++;
+        }
+
+        // Both entry and file are equal, check the modification date
+        else
+        {
+          // [Hack] When (re)moving the file from its original place, Chrome
+          // show it with size = 0 and lastModifiedDate = null instead of
+          // raising a NotFoundError error
+          if(file.lastModifiedDate == null)
+            filesManager.delete(entry)
+
+          else if(entry.lastModifiedDate < file.lastModifiedDate)
+            filesManager.add(file)
+
+          ai++;
+          bi++;
+        }
+      }
+
+      setTimeout(function()
+      {
+        self.hash()
+      },
+      5*1000)
+//      1*60*1000)
+    })
+  }
+
   filesManager.addEventListener('file.added', function(event)
   {
     var fileentry = event.fileentry
@@ -55,6 +130,7 @@ _priv.Sharedpoint = function(db, filesManager)
     }
   });
 }
+
 
 _priv.SharedpointsManager = function(db)
 {
